61ec9791979496675d7675ac2653a01c3d4eb129
alrodsa <alrodsa2@inf.upv.es>
2025-08-16 21:07:42 +0000

    feat: refactor Namer class to utilize Processor for file copying and metadata replacement

--- src/core/namer.py
+++ src/core/namer.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-import shutil
-
+from src.core.processor import Processor
 from src.data.folder import Folder
 from src.common.enums import NamingMode
 
@@ -11,29 +10,16 @@ class Namer:
     mode: NamingMode
     in_image: bool
 
-    def _copy_folder(
-        self, folder: Folder, source_root: Path, output_root: Path
-    ) -> None:
-
-        relative_path = Path(folder.directory).relative_to(source_root)
-        dest_dir = output_root / relative_path
-        dest_dir.mkdir(parents=True, exist_ok=True)
-
-        for file in folder.files:
-            src_file = Path(file.directory)
-            dst_file = dest_dir / file.name
-            shutil.copy2(src_file, dst_file)
-
-        for subfolder in folder.folders:
-            self._copy_folder(subfolder, source_root, output_root)
-
     def run(self, folder: Folder, output_dir: str) -> "Namer":
-        print ("Running namer")
-        print(self.mode == NamingMode.COPY)
         if self.mode == NamingMode.COPY:
-            print(f"Copying files from {folder.directory} to {output_dir}")
-            self._copy_folder(folder, Path(folder.directory), Path(output_dir))
-
-
-
-
+            Processor.copy_naming_metadata(
+                folder, Path(folder.directory), Path(output_dir), in_image=self.in_image
+            )
+            print(f"📂 Copied files from {folder.directory} ➝ {output_dir}...")
+        elif self.mode == NamingMode.REPLACE:
+            Processor.replace_naming_metadata(folder, in_image=self.in_image)
+            print(f"✍️ Replaced files in {folder.directory} with photo metadata info.")
+        else:
+            raise ValueError(f"Unsupported naming mode: {self.mode}")
+    
+        return self
